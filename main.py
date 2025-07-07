db198d9f686f543d6b000e3d8292cab9eeda344b
SimaoGoncalves99 <simaocup@gmail.com>
2025-07-07 12:15:06 +0000

    data transformation

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from src.datascience.pipeline.data_ingestion_pipeline import (
 from src.datascience.pipeline.data_validation_pipeline import (
     DataValidationTrainingPipeline,
 )
+from src.datascience.pipeline.data_transformation_pipeline import (
+    DataTransformationTrainingPipeline,
+)
+
 
 logger.info("Welcome to our custom logging datascience package!")
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.initiate_data_ingestion()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx======x")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -22,10 +26,21 @@ except Exception as e:
 STAGE_NAME = "Data Validation Stage"
 
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
     obj.initiate_data_validation()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx======x")
 except Exception as e:
     logger.exception(e)
     raise e
